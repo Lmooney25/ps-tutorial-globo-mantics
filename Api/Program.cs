46e67be33f166257a533cab68ecdb85ae9ec5325
Liam Mooney <liam.mooney@endjin.com>
2024-08-28 12:02:54 +0100

    Configure CORS

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 builder.Services.AddDbContext<HouseDbContext>(
     o => o.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
 );
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
 
 app.UseHttpsRedirection();
 
