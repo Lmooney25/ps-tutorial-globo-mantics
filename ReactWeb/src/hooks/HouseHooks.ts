aeea136e8a84c1b270a194df7d0abb2e2e36aebc
Liam Mooney <liam.mooney@endjin.com>
2024-09-07 22:09:46 +0100

    Add navigation client-side navigation and house detail page

--- ReactWeb/src/hooks/HouseHooks.ts
+++ ReactWeb/src/hooks/HouseHooks.ts
@@ -3,6 +3,23 @@ import config from "../config";
 import axios, { AxiosError } from "axios";
 import { useQuery } from "@tanstack/react-query";
 
+// const useFetchHouses = (): House[] => {
+//     const [houses, setHouses] = useState<House[]>([]);
+
+//     // Fetch houses from the API when the component mounts/ first appears on the screen.
+//     useEffect(() => {
+//         const fetchHouses = async () => {
+//             const rsp = await fetch(`${config.baseApiUrl}/houses`);
+//             const houses = await rsp.json();
+//             setHouses(houses);
+//         }
+
+//         fetchHouses();
+//     }, []);
+
+//     return houses;
+// }
+
 const useFetchHouses = () => {
 
     return useQuery<House[], AxiosError>({
@@ -13,4 +30,13 @@ const useFetchHouses = () => {
 
 }
 
+const useFetchHouse = (id: number) => {
+    return useQuery<House, AxiosError>({
+        queryKey: ["house", id],
+        queryFn: () => 
+            axios.get(`${config.baseApiUrl}/houses/${id}`).then((rsp) => rsp.data),
+    })
+}
+
 export default useFetchHouses;
+export { useFetchHouse };
